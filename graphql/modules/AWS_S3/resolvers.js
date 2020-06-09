e1781aabe902de6fdc6d93310362a470df7d5605
RagOfJoes <vhsvragojos@gmail.com>
2020-06-09 02:14:37 -0700

    MOD: change AWS env keys

--- graphql/modules/AWS_S3/resolvers.js
+++ graphql/modules/AWS_S3/resolvers.js
@@ -8,6 +8,8 @@ module.exports = {
 			const s3 = new AWS.S3({
 				region: 'us-west-1',
 				signatureVersion: 'v4',
+				accessKeyId: process.env.AWS_KEY_ID,
+				secretAccessKey: process.env.AWS_KEY_SECRET,
 			});
 
 			const params = {
@@ -31,8 +33,8 @@ module.exports = {
 			const s3 = new AWS.S3({
 				region: 'us-west-1',
 				signatureVersion: 'v4',
-				accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-				secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+				accessKeyId: process.env.AWS_KEY_ID,
+				secretAccessKey: process.env.AWS_KEY_SECRET,
 			});
 
 			try {
